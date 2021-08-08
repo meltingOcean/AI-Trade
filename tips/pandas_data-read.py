d9abbf5c477f0416bd210ed05775cf55d6dcc998
정 승혁 <tmdgur325@naver.com>
2021-08-08 14:25:30 +0000

    add content to data-read

--- tips/pandas_data-read.py
+++ tips/pandas_data-read.py
@@ -8,5 +8,11 @@
 
 import pandas as pd
 
-path = ''                    # path of csv file
+path = ''   # path of csv file
 DataFrame = pd.read_csv(path)
+print(DataFrame.head())
+
+DataFrame = pd.read_csv(path, index_col='Date', parse_dates=['Date'])
+# Option 'index_col' define index colomn
+# Option 'parse_dates' convert 'str' type column to 'timestamp' type
+print(DataFrame.head())
