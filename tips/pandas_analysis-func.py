3ffc341ae5c943030a492bddcd27ea6071364ce3
정 승혁 <tmdgur325@gmail.com>
2021-08-16 13:36:51 +0000

    add fuction is pandas_anlaysis-func.py

--- tips/pandas_analysis-func.py
+++ tips/pandas_analysis-func.py
@@ -12,3 +12,18 @@ dataframe = pd.read_csv(path, index_col='Date', parse_dates=['Date'])
 # default periods is 1
 dataframe['pct_change'] = dataframe['Close'].pct_change(periods=1)
 print(dataframe)
+
+# diff() function
+# calcuate difference between on period
+# default periods is 1
+
+dataframe['close_diff'] = dataframe['Close'].diff(periods=1)
+print(dataframe)
+
+# rolling() function
+# generate mean, max, min value in window size
+
+dataframe['MovingAverage'] = dataframe['Close'].rolling(window=4).mean()
+dataframe['Maximum'] = dataframe['Close'].rolling(window=4).max()
+dataframe['Minimum'] = dataframe['Close'].rolling(window=4).min()
+print(dataframe)
