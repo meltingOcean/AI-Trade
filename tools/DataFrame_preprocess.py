2fe6edcf695a2db38b97a1676617fc8d19105770
정 승혁 <tmdgur325@gmail.com>
2021-08-16 13:22:25 +0000

    z

--- tools/DataFrame_preprocess.py
+++ tools/DataFrame_preprocess.py
@@ -6,7 +6,9 @@ def del_NaN_padbfill(dataframe):
     dataframe.fillna(method='pad', inplace=True)
     dataframe.fillna(method='bfill', inplace=True)
 
+
 def del_NaN_bfillpad(dataframe):
     dataframe.replace([np.inf, -np.inf], np.nan, inplace=True)
     dataframe.fillna(method='bfill', inplace=True)
     dataframe.fillna(method='pad', inplace=True)
+    
